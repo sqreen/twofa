1d9da862959c17d76a8f641d17c969806b110670
Jānis Kiršteins <janis@sqreen.io>
2019-05-12 00:38:59 +0200

    Fixed syntax error in build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ EXECUTABLE="./.build/x86_64-apple-macosx/release/TwoFa"
 
 swift build \
 -c release \
---static-swift-stdlib
+--static-swift-stdlib \
 -Xlinker -sectcreate \
 -Xlinker __TEXT -Xlinker __info_plist -Xlinker Supporting/Info.plist
 
