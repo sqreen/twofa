7386b7f86d3a87de76872a732bc0a2ddb4c5a706
Jānis Kiršteins <janis@sqreen.com>
2020-06-20 19:08:16 +0200

    Build script fixes

--- build_mac.sh
+++ build_mac.sh
@@ -2,14 +2,16 @@
 set -e
 
 CONFIG="release"
-EXECUTABLE="$(find .build -name TwoFa | grep $CONFIG | grep -v dSYM | grep macosx)"
 
 BUILDOUT="$(swift build \
 -c $CONFIG \
---static-swift-stdlib \
 -Xlinker -sectcreate \
 -Xlinker __TEXT -Xlinker __info_plist -Xlinker Supporting/Info.plist)"
+
+EXECUTABLE="$(find .build -name TwoFa | grep $CONFIG | grep -v dSYM | grep macosx)"
 
 ID="Developer ID Application: Notakey Latvia, SIA (N2JEMR5FZG)"
 
 codesign --entitlements "Supporting/twofa.entitlements" -s "$ID" "$EXECUTABLE"
+
+mv -v "$EXECUTABLE" "${EXECUTABLE%TwoFa}twofa"
