e6aaddef38219bb446da83a2875b0f87e5c894bd
Jānis Kiršteins <janis@sqreen.com>
2020-06-20 19:16:51 +0200

    Clearer script output for build_mac.sh

--- build_mac.sh
+++ build_mac.sh
@@ -2,16 +2,22 @@
 set -e
 
 CONFIG="release"
+ID="Developer ID Application: Notakey Latvia, SIA (N2JEMR5FZG)"
 
+echo "==> Building"
 BUILDOUT="$(swift build \
 -c $CONFIG \
 -Xlinker -sectcreate \
 -Xlinker __TEXT -Xlinker __info_plist -Xlinker Supporting/Info.plist)"
 
 EXECUTABLE="$(find .build -name TwoFa | grep $CONFIG | grep -v dSYM | grep macosx)"
+echo "    built: $EXECUTABLE"
 
-ID="Developer ID Application: Notakey Latvia, SIA (N2JEMR5FZG)"
-
+echo "==> Codesigning"
+echo "    identity: $ID"
 codesign --entitlements "Supporting/twofa.entitlements" -s "$ID" "$EXECUTABLE"
+echo "    done"
 
-mv -v "$EXECUTABLE" "${EXECUTABLE%TwoFa}twofa"
+echo "==> Renaming output"
+echo "    $EXECUTABLE -> ${EXECUTABLE%TwoFa}twofa"
+mv "$EXECUTABLE" "${EXECUTABLE%TwoFa}twofa"
